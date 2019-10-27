86091d5d42f2c7654197ac9beb32903f907f2db6
Aras Emami <aras.armani2014@gmail.com>
2019-10-27 16:19:08 +0330

    fixed observable event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-observable';
+  observable = Observable.create((observer) => {
+    observer.next('start processing . . .');
+
+    setTimeout(() => {
+      observer.next('still processing...');
+    }, 2000);
+
+    setTimeout(() => {
+      observer.complete();
+    }, 5000);
+
+  });
+
+  ngOnInit() {
+    this.observable.subscribe(
+      data => console.log(data),
+      error => console.log(error),
+      () => console.log('process is finish!')
+
+    );
+  }
 }
